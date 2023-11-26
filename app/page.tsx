53c1767c14c7e59ab2bbb329aae4ee8e760f7709
David Temitope <Temi236topemicheal@gmail.com>
2023-11-26 11:58:55 +0100

    Feat:Final Completation of project

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,5 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
-  if (!session) {
-    return redirect("/login");
-  } else {
-    return redirect("/Home");
-  }
+  return redirect("/Home");
 }
